c5110fdf33938497cd7c843655f5bb2358562308
Ivan Fish <fridey0@gmail.com>
2019-11-20 10:35:53 +0400

    Сделал блоки до "Галлерии"

---
+++ src/app/jk-objects/jk-objects-item/object-item-gallery/object-item-gallery.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-object-item-gallery',
+    templateUrl: 'object-item-gallery.component.html',
+    styleUrls: [
+        'object-item-gallery.component.scss',
+        '../jk-objects-item.component.scss'
+    ]
+})
+
+export class ObjectItemGalleryComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
--- src/app/jk-objects/jk-objects.module.ts
+++ src/app/jk-objects/jk-objects.module.ts
@@ -1,21 +1,42 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
+
 import { JkObjectsComponent } from './jk-objects.component';
 import { JkObjectsListComponent } from './jk-objects-list/jk-objects-list.component';
 import { JkObjectsItemComponent } from './jk-objects-item/jk-objects-item.component';
+import { ObjectItemPreviewComponent } from './jk-objects-item/object-item-preview/object-item-preview.component';
+import { ObjectItemFilterComponent } from './jk-objects-item/object-item-filter/object-item-filter.component';
+import { GHMRangeNumberModule } from '../flats/search/search-form/ghm-range-number/ghm-range-number.module';
+import { GHMRangeNumberComponent } from '../flats/search/search-form/ghm-range-number/ghm-range-number.component';
+import { ObjectItemTriggerComponent } from './jk-objects-item/object-item-trigger/object-item-trigger.component';
+import { ObjectItemStorageComponent } from './jk-objects-item/object-item-storage/object-item-storage.component';
+import { ObjectItemBuilderComponent } from './jk-objects-item/object-item-builder/object-item-builder.component';
+import { ObjectItemFlatComponent } from './jk-objects-item/object-item-flat/object-item-flat.component';
+import { ObjectItemGalleryComponent } from './jk-objects-item/object-item-gallery/object-item-gallery.component';
 
 const JkObjectsComponents = [
   JkObjectsComponent,
   JkObjectsListComponent,
-  JkObjectsItemComponent
+  JkObjectsItemComponent,
+  ObjectItemPreviewComponent,
+  ObjectItemFilterComponent,
+  ObjectItemTriggerComponent,
+  ObjectItemStorageComponent,
+  ObjectItemBuilderComponent,
+  ObjectItemFlatComponent,
+  ObjectItemGalleryComponent
 ];
 
 @NgModule({
-  exports: [...JkObjectsComponents],
+  exports: [
+    ...JkObjectsComponents,
+    GHMRangeNumberComponent
+  ],
   declarations: [...JkObjectsComponents],
   imports: [
     CommonModule,
+    GHMRangeNumberModule,
     RouterModule.forChild([
       { path: '', component: JkObjectsComponent
         , children : [
