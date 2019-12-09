53e8a79622d4a6a0349e7db5e123fae4261b25c7
Ivan Fish <fridey0@gmail.com>
2019-12-09 17:28:08 +0400

    start dynamic page

---
+++ src/app/jk-objects/jk-objects-item/object-dynamic/object-dynamic.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-object-dynamic',
+    templateUrl: 'object-dynamic.component.html'
+})
+
+export class ObjectDynamicComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
