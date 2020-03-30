97cd9b36d9ef8a76aef8685c178bd206f202c046
Lyapunov Mihail <lyapunovmihail@gmail.com>
2020-03-30 15:09:40 +0400

    Делаю объекты

--- src/app/jk-objects/jk-objects-list/objects-map/objects-map.component.ts
+++ src/app/jk-objects/jk-objects-list/objects-map/objects-map.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { JkObjectsListService } from '../jk-objects-list.service';
-import { IObjectSnippet } from '../../../../../serv-files/serv-modules/jk-objects/object-api/objects.interfaces';
+import { IObjectSnippet, OBJECTS_UPLOADS_PATH } from '../../../../../serv-files/serv-modules/jk-objects/object-api/objects.interfaces';
 declare let ymaps: any;
 
 @Component({
@@ -16,8 +16,11 @@ export class ObjectsMapComponent implements OnInit {
     @Input()
     public snippets: IObjectSnippet[];
 
+    public markers = [];
+
+    public uploadsPath = `/${OBJECTS_UPLOADS_PATH}`;
+
     constructor(
-        private objectsListService: JkObjectsListService
     ) {
     }
 
@@ -26,48 +29,57 @@ export class ObjectsMapComponent implements OnInit {
     }
 
     private initMap() {
-        ymaps.ready(function() {
-            const myMap = new ymaps.Map('map', {
-                    center: [55.751574, 37.573856],
-                    zoom: 9,
-                    controls: ['zoomControl']
-                }),
 
+        const that = this;
 
-                MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-                    '<img src="/assets/img/mock/map-obj/obj1.jpg" class="img-map">'
-                ),
-                myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
-                        iconContent: ''
-                    }
-                    , {
+        ymaps.ready(() => {
 
-                        iconLayout: 'default#imageWithContent',
-
-                        iconImageHref: '',
-
-                        iconImageSize: [64, 64],
-
-                        iconImageOffset: [-24, -24],
-
-                        iconContentOffset: [15, 15],
-
-                        iconContentLayout: MyIconContentLayout
+                const myMap = new ymaps.Map('map', {
+                        center: [55.751574, 37.573856],
+                        zoom: 9,
+                        controls: ['zoomControl']
                     });
-            myMap.behaviors.disable(['scrollZoom']);
-            myMap.geoObjects
-            // .add(myPlacemark)
-                .add(myPlacemarkWithContent);
 
-            myPlacemarkWithContent.events
-                .add('mouseenter', function(e) {
-                    // Ссылку на объект, вызвавший событие,
-                    // можно получить из поля 'target'.
-                    // e.get('target').options.set('preset', 'islands#greenIcon');
-                    document.querySelector('.img-map').classList.add('img-map--hover');
-                })
-                .add('mouseleave', function(e) {
-                    document.querySelector('.img-map').classList.remove('img-map--hover');
+                this.snippets.forEach((item, index) => {
+
+                    this.markers[index] = {};
+                    this.markers[index].click = false;
+
+                    const MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+                        `<img src="${this.uploadsPath + item.thumbnail}" id="img-map-${index}" class="img-map" width="64" height="64">`
+                    );
+
+                    this.markers[index].marker = new ymaps.Placemark(item.coords.split(','), {
+                            iconContent: ''
+                        }, {
+
+                            iconLayout: 'default#imageWithContent',
+
+                            iconImageHref: '',
+
+                            iconImageSize: [64, 64],
+
+                            iconImageOffset: [-24, -24],
+
+                            iconContentOffset: [15, 15],
+
+                            iconContentLayout: MyIconContentLayout
+                        });
+                    myMap.behaviors.disable(['scrollZoom']);
+                    myMap.geoObjects
+                    // .add(myPlacemark)
+                        .add(this.markers[index].marker);
+
+                    this.markers[index].marker.events
+                        .add('mouseenter', function(e) {
+                            // Ссылку на объект, вызвавший событие,
+                            // можно получить из поля 'target'.
+                            // e.get('target').options.set('preset', 'islands#greenIcon');
+                            document.querySelector(`#img-map-${index}`).classList.add('img-map--hover');
+                        })
+                        .add('mouseleave', function(e) {
+                            document.querySelector(`#img-map-${index}`).classList.remove('img-map--hover');
+                        });
                 });
         });
     }
