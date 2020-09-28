27bf26096a1a5f340f086b0ff1ff0c28648151ed
Maxim Frolov <buterbrodhs@gmail.com>
2020-09-28 12:05:46 +0400

    RED-ilike: настроил компонент

---
+++ src/app/home/ilike/home-ilike.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-home-ilike',
+    templateUrl: 'home-ilike.component.html'
+})
+
+export class HomeIlikeComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
